70f369c32563865df8f9b9ef4d8d37a6aafec0a0
Eman <aymanmjdyabwjbl@gmail.com>
2025-01-25 14:32:19 +0200

    Delete unused imports2

--- app/src/main/java/com/example/availableroutecourses/SplashActivity.java
+++ app/src/main/java/com/example/availableroutecourses/SplashActivity.java
@@ -6,12 +6,8 @@ import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
