a0abece1a420533f0a3d40044a2f9463241be4e4
Eman <aymanmjdyabwjbl@gmail.com>
2025-01-25 13:29:57 +0200

    Remove space

--- app/src/main/java/com/example/availableroutecourses/CoursesActivity.java
+++ app/src/main/java/com/example/availableroutecourses/CoursesActivity.java
@@ -6,9 +6,7 @@ import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import com.example.availableroutecourses.utilits.Constantes;
 
@@ -19,10 +17,9 @@ public class CoursesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_courses);
-
     }
     public void onViewClick(View view ){
-        if(view.getId()==R.id.android_imv||view.getId()==R.id.android_btn){
+        if(view.getId()==R.id.android_imv||view.getId()==R.id.android_btn){   //commit
             naviigateToCourseDetails(Constantes.ANDROID_COURSES);
             return;
         }
@@ -34,7 +31,6 @@ public class CoursesActivity extends AppCompatActivity {
             naviigateToCourseDetails(Constantes.FULL_STACK_COURSES);
         }
     }
-
     private void naviigateToCourseDetails( String course) {
         Intent intent =new Intent(this,CourseDetailsActivity.class);
         intent.putExtra(Constantes.COURSES,course);
